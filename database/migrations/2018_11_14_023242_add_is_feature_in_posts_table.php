3b5cd8d6187850371be89995c84b0a9b4c6c602e
3a532008 <isme1788@gmail.com>
2018-11-14 10:34:27 +0800

    運用 migration 調整資料表結構

--- database/migrations/2018_11_14_023242_add_is_feature_in_posts_table.php
+++ database/migrations/2018_11_14_023242_add_is_feature_in_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class AddIsFeatureInPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->text('content');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            //
         });
     }
 
@@ -28,6 +25,11 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->string('title',500)->change();
+            $table->boolean('is_feature')->default(false)->after('content');
+
+        });
     }
 }
